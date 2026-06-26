e762d55d6e10986834be56d1d35be843f184f057
Kush Kumar Kasaudhan <kush.kasaudhan@CELSYSWTC.IN>
2026-06-26 23:05:39 +0530

    DDT Login

--- src/test/java/testcases/TC_003_LoginDDT.java
+++ src/test/java/testcases/TC_003_LoginDDT.java
@@ -10,24 +10,60 @@ Data is invalid - login success - test fail  - logout
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import pageObjects.HomePage;
 import pageObjects.LoginPage;
 import testbase.BaseClass;
+import utilities.DataProviders;
 
 public class TC_003_LoginDDT extends BaseClass {
 	
-	@Test
-	public void verify_LoginDDT() {
+	@Test(dataProvider="LoginData", dataProviderClass=DataProviders.class) //getting data provider from different class
+	public void verify_LoginDDT(String email, String pwd, String exp) {
 		
 		logger.info("********Starting TC_003_LoginDDTTestCase********");
 		try
 		{
 		LoginPage lp = new LoginPage(driver);
 		
+		lp.EnterEmail(p.getProperty("Email"));
+		logger.info("Entered Email");
+		
+		lp.EnterPassword(p.getProperty("Password"));
+		logger.info("Entered Password");
+		
+		lp.ClickLogin();
+		logger.info("Clicked Login Button");
+		
+		String title= driver.getTitle();
 		
 		
+		HomePage hp = new HomePage(driver);
 		
-
+		if(exp.equalsIgnoreCase("Valid"))
+		{
+			if(title.equals("Let's Shop"))
+			{
+				hp.ClickLogout();
+				Assert.assertTrue(true);
+			}
+			else
+			{
+				Assert.assertTrue(false);
+			}
 		}
+		if(exp.equalsIgnoreCase("Invalid"))
+		{
+			if(title.equals("Let's Shop"))
+			{
+				hp.ClickLogout();
+				Assert.assertTrue(false);
+			}
+			else
+			{
+				Assert.assertTrue(true);
+			}
+		}
+	}
 		catch(Exception e)
 		{
 			logger.error("Test Failed.......");
